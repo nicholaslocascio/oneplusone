a64d36675d7fda4e0e6c63782a88e03dc8fbd95c
indydev <nicholasjlocascio@gmail.com>
2013-10-18 13:06:01 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneplusone::Application.config.secret_token = '385f98dae8a255d5ce872807ac6b00c78bfc38658e9c716ab90b305af7232ed7d81bc8f33a7cb1d409e34ea8cb993ad09f0c19f94bc75c2c4d56e00706eefe0e'
