55c6de4c80480d807481fd664e7e2b7336525e21
indydev <nicholasjlocascio@gmail.com>
2013-10-20 12:05:24 -0400

    Styled team edit page.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ActiveRecord::Base
   attr_accessible :name
   belongs_to :organization
+
+  validates :name, length: {minimum: 1, maximum: 36}, on: :update
+  validates :name, length: {minimum: 1, maximum: 36}, on: :create
+
 end
