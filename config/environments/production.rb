924a5f4f6ac0bf7bdf271cf5908a658dbe15dd29
indydev <nicholasjlocascio@gmail.com>
2013-10-21 14:15:35 -0400

    Fixed action mailer send config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,14 @@ Oneplusone::Application.configure do
   # Do care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+
   # Change mail delvery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "google.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: 'oneplusonenjl',
